6dc370fa3d48d6df91fe3cbed079f47d1ab05438
Matheus Pedro <matheus.pedro1214@gmail.com>
2021-02-05 23:10:09 -0300

    CRUD Produto

--- src/Server.js
+++ src/Server.js
@@ -1,15 +1,40 @@
 const expres = require('express');
+const app = expres()
 const porta = 3003
 
-const app = expres()
+const bd = require('./BD');
+const bodyParser = require('body-parser');
 
-app.get('/produtos', (req, res) => {
-    res.send({
-        nome: 'Notebook', 
-        preco: 123.45
+app.use(bodyParser.urlencoded( { extended: true } ))
+
+app.post('/produtos', (req, res) => {
+    const produto = bd.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
     })
+    res.send(produto)
 })
 
+app.get('/produtos', (req, res) => {
+    res.send(bd.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res) => {
+    res.send(bd.getProduto(req.params.id))
+})
+
+app.put('/produtos/:id', (req, res) => {
+    const produto = bd.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco,
+        id: req.params.id,
+    })
+    res.send(produto)
+})
+
+app.delete('/produtos/:id', (req, res) => {
+    res.send(bd.excluirProduto(req.params.id))
+})
 
 app.listen(porta, () => {
     console.log(`Servidor está executando na porta ${porta}.`)
